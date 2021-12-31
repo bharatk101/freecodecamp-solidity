0f8b9ca3d71d131a6b0a409bd40cb0261118e7f3
bharatk101 <kulkarnibharat018@gmail.com>
2021-12-31 18:45:03 +0530

    Completed aave brownie protocol implementation

--- aave_brownie/scripts/aave_borrow.py
+++ aave_brownie/scripts/aave_borrow.py
@@ -19,6 +19,58 @@ def main():
                               account.address, 0, {"from": account})
     tx.wait(1)
     print("Deposited!")
+    # get borrowable data
+    borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+    # DAI to eth price
+    dai_eth_price = get_assest_price(
+        config['networks'][network.show_active()]['dai_eth_price_feed'])
+    amount_dai_to_borrow = (1/dai_eth_price) * (borrowable_eth * 0.95)
+    print(f"We are going to borrow {amount_dai_to_borrow}")
+    # borrow
+    dai_address = config['networks'][network.show_active()]['dai_token']
+    borrow_tx = lending_pool.borrow(dai_address, Web3.toWei(
+        amount_dai_to_borrow, "ether"), 1, 0, account.address, {"from": account})
+    borrow_tx.wait(1)
+    print("we borrowed some DAI")
+    get_borrowable_data(lending_pool, account)
+    repay_all(amount, lending_pool, account)
+
+
+def repay_all(amount, lending_pool, account):
+    approve_erc20(Web3.toWei(amount, "ether"),
+                  lending_pool,
+                  config['networks'][network.show_active()]['dai_token'],
+                  account
+                  )
+
+    repay_tx = lending_pool.repay(config['networks'][network.show_active(
+    )]['dai_token'], amount, 1, account.address, {"from": account})
+    repay_tx.wait(1)
+    print("Repayed!")
+
+
+def get_assest_price(price_feed_address):
+    dai_eth_price_feed = interface.AggregatorV3Interface(price_feed_address)
+    latest_price = dai_eth_price_feed.latestRoundData()[1]
+    converted_price = Web3.fromWei(latest_price, "ether")
+    print(f"Dai-Eth price is {converted_price}")
+    return float(converted_price)
+
+
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidation_threshold,
+        ltv,
+        health_factor) = lending_pool.getUserAccountData(account)
+
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+    print(available_borrow_eth, total_collateral_eth, total_debt_eth)
+    return (float(available_borrow_eth), float(total_debt_eth))
 
 
 def approve_erc20(amount, spender, erc20_address, account):
